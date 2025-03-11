225296d66c5a8d007d75ad521f2327b396311803
song huang <1246474700@qq.com>
2025-03-11 16:41:59 +0800

    Delete sourceCode/gen_rpt.sh

--- sourceCode/gen_rpt.sh
+++
@@ -1,9 +0,0 @@
-#!\bin\bash
-d = $(date +%y%m%d)
-hive -e "use mydb;insert overwrite table comment_list partition (dt=${d})
-select 
-ctime as comtime,
-message as content 
-from bilicomment 
-where dt=${d}
-order by comtime desc;"
