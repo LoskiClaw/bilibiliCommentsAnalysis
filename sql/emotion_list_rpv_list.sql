d65209a1817db271a43ec04f5bea6739407f86cb
song huang <1246474700@qq.com>
2025-03-11 16:32:18 +0800

    Add files via upload

---
+++ sql/emotion_list_rpv_list.sql
@@ -0,0 +1,4 @@
+INSERT INTO rpv_list.emotion_list (emotiontype,count_num) VALUES
+	 ('平和',33404),
+	 ('积极',4988),
+	 ('消极',13390);
---
+++ sql/users_rpv_list.sql
@@ -0,0 +1,3 @@
+INSERT INTO rpv_list.users (usrname,pwd,usertype) VALUES
+	 ('admin','123456','root'),
+	 ('root','123456','normal');
